0ba7f8ce58a353679a128d981714b0ea390d1f74
FKAYSR <kejserfreja@gmail.com>
2026-07-27 01:39:00 +0200

    feat: update project name and base path to portfolio-freja-kejser

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio-2026/",
+  base: "/portfolio-freja-kejser/",
   plugins: [react()],
 });
